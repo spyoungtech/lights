db034391fe75938b6d7d8837f3a6de2c5d6844d0
Spencer Young <spencer.young@spyoung.com>
2020-01-05 20:54:44 -0800

    let there be lights

--- lights/__init__.py
+++ lights/__init__.py
@@ -27,14 +27,69 @@ class Lights(object):
         for light in self._lights:
             light.on = False
 
-    def brighter(self, increment=10, group=None):
+    def brighter(self, increment=10, light=None, group=None):
+        if light:
+            l = self._lights[light]
+            l.brightness = l.brightness - increment
+            return
+        if group:
+            g = self._bridge.groups[group]
+            g.brightness = g.brightness - increment
         for light in self._lights:
             light.brightness = light.brightness + increment
 
-    def dim(self, increment=10, group=None):
+    def dim(self, increment=10, light: int=None, group: int=None):
+        """
+        Dim the lights. A light or group can be specified. If no light or group is specified, dims all lights.
+
+        ex::
+
+            lights dim --light=1
+
+
+        :param light: the light number to dim. (optional)
+        :param increment: change in brightness
+        :param group: the group number to dim. (optional)
+        :return:
+        """
+        if light:
+            l = self._lights[light]
+            l.brightness = l.brightness - increment
+            return
+        if group:
+            g = self._bridge.groups[group]
+            g.brightness = g.brightness - increment
         for light in self._lights:
             light.brightness = light.brightness - increment
 
+    def brightness(self, new_brightness=None, light=None, group=None):
+        """
+        Check or change the brightness of the lights. A light or group can be specified. If no light or group is
+        specified, dims all lights.
+
+        :param new_brightness:
+        :param light:
+        :param group:
+        :return:
+        """
+
+    def list(self):
+        """
+        List the lights connected to the phillips hue bridge
+
+        :return:
+        """
+        for index, light in enumerate(self._lights, start=1):
+            print(index, light)
+
+    def groups(self):
+        """
+        List the light groups on the Phillips Hue Bridge
+
+        :return:
+        """
+        for index, group in enumerate(self._bridge.groups):
+            print(index, group)
 
 def main():
     fire.Fire(Lights)
